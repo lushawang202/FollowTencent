000ec4fc2ea77abeec635b11ddb57811a1deab55
lushawang202 <lucia111202@126.com>
2020-05-19 11:13:43 +0800

    first commit

---
+++ Testing/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__=['demo2','demo3']
---
+++ Testing/demo2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+hello = "Hello demo2"
+
+
+def f():
+    print("function f()")
+
+
+class Demo2:
+    pass
